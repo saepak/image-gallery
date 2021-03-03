04469b5cb583a046f9350866090d46abfb264ade
saebyeolp <saebyeolpak@gmail.com>
2021-03-03 11:56:16 -0800

    added search

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -2,6 +2,10 @@ import React from 'react';
 
 const ImageCard = ({ image }) => {
 
+  const tags = image.tags.split(',')
+  const views = image.views.toLocaleString()
+  const downloads = image.views.toLocaleString()
+  const likes = image.likes.toLocaleString()
 
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg">
@@ -13,17 +17,26 @@ const ImageCard = ({ image }) => {
         <ul>
           <li>
             <strong>Views: </strong>
-            {image.views}
+            {views}
           </li>
           <li>
             <strong>Downloads: </strong>
-            {image.downloads}
+            {downloads}
           </li>
           <li>
             <strong>Likes: </strong>
-            {image.likes}
+            {likes}
           </li>
         </ul>
+      </div>
+      <div className='px-6 py-4'>
+        {tags.map((tag, index)=>(
+          <span key={index} className='inline-block mb-1 bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2'>
+            #{tag}
+          </span>
+        ))
+
+        }
       </div>
     </div>
   )
